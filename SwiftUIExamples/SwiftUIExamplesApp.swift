f79c20cc42473f2c263de3b984b0b0c0399b04b7
SeymaArslan <seymarslannn@gmail.com>
2024-12-31 15:55:08 +0300

    Initial Commit

---
+++ SwiftUIExamples/SwiftUIExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIExamplesApp.swift
+//  SwiftUIExamples
+//
+//  Created by Seyma Arslan on 31.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
